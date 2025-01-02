bebc69b43a83db4837d42e3fbc9423e839f778c1
Osvaldo Melo <omelo@redhat.com>
2025-01-02 17:24:31 -0300

    Deleted sensible files

--- camel-quarkus-soap-kafka/src/main/java/br/com/solarbr/opshml/apps/kafka_soap_api_dev_ri_0_solar_core_apis_dev/kafka/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://kafka-soap-api-dev-ri-0-solar-core-apis-dev.apps.opshml.solarbr.com.br/kafka")
-package br.com.solarbr.opshml.apps.kafka_soap_api_dev_ri_0_solar_core_apis_dev.kafka;
